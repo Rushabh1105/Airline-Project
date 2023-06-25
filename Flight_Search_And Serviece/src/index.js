8fa1a98cccc41e7599f5a2368b15ae3a82591194
Rushabh1105 <rushabh.sakharkar1998@gmail.com>
2023-06-26 02:06:45 +0530

    basic folder setup and ORM setup

--- Flight_Search_And Serviece/src/index.js
+++ Flight_Search_And Serviece/src/index.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
+
 const { PORT } = require("./configs/serverConfig");
 
 
@@ -6,7 +9,10 @@ const setupAndStartServer = async () => {
     // create the express object
     const app = express();
 
-    // 
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    // start the server
     app.listen(PORT, () => {
         console.log(`Server Started on ${PORT}`);
         //console.log(process.env);
