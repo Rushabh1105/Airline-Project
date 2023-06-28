d453310cedc83e1c2a58f0e393f1dac40eb2c43e
Rushabh1105 <rushabh.sakharkar1998@gmail.com>
2023-06-28 18:25:16 +0530

    added middleware to check signin and signup

---
+++ Auth_Serviece/src/middlewares/auth-request-validator.js
@@ -0,0 +1,17 @@
+const validateUserSignup = (req, res, next) => {
+    if (!req.body.email || !req.body.password) {
+        return res.status(400).json({
+            data: {},
+            success: false,
+            message: 'Bad request',
+            err: 'Missing email or password',
+        })
+    }
+
+    next();
+}
+
+module.exports = {
+    validateUserSignup,
+
+}
--- Auth_Serviece/src/routes/V1/auth-routes.js
+++ Auth_Serviece/src/routes/V1/auth-routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
 const UserController = require('../../controllers/user-controller');
+const { AuthRequestValidator } = require('../../middlewares/index')
 
 const router = express.Router();
 
-router.post('/signup', UserController.createUser);
-router.post('/signin', UserController.signIn)
+router.post('/signup', AuthRequestValidator.validateUserSignup, UserController.createUser);
+router.post('/signin', AuthRequestValidator.validateUserSignup, UserController.signIn)
 
 module.exports = router;
