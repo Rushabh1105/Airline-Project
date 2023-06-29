bbd11831b659789ce744c85c1c64a633380d2458
Rushabh1105 <rushabh.sakharkar1998@gmail.com>
2023-06-29 18:32:36 +0530

    basic setup done for booking serviece

---
+++ Booking_Serviece/src/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { PORT, DB_SYNC } = require('./config/server-config');
+const apiRoutes = require('./routes/index');
+const db = require('./models/index');
+
+const app = express();
+
+const setupAndStartServer = () => {
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use('/api', apiRoutes);
+
+    app.listen(PORT, () => {
+        console.log(`Server started on port ${PORT}`);
+
+        if (DB_SYNC) {
+            db.sequelize.sync({ alter: true })
+        }
+    })
+}
+
+setupAndStartServer();
+
+
+
